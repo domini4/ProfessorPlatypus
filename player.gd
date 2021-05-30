bce20b63b7879266931de38547c592cb543a6076
James Dominic <domini4@clemson.edu>
2021-05-30 11:59:38 -0400

    Added player control script

--- player.gd
+++ player.gd
@@ -1,16 +1,17 @@
 extends RigidBody2D
 
+var started = false
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func _physics_process(delta):
+	if Input.is_action_just_pressed("swim"):
+		if !started:
+			start_swim()
+		swim_speed()
 
+func start_swim():
+	started = true
+	gravity_scale = 5.0
+	$AnimationPlayer.play("swim")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func swim_speed():
+	linear_velocity.y = -200
