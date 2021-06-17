4d86e8fa6ae9a2e983230902a92a6b792199f80d
James Dominic <domini4@clemson.edu>
2021-06-17 14:03:59 -0400

    rock added

--- player.gd
+++ player.gd
@@ -2,20 +2,28 @@ extends RigidBody2D
 
 var started = false
 
+var playMusic = false
+
 func _physics_process(delta):
+	##print(position.y)
+
+	if !playMusic:
+		$Music.play()
+		playMusic = true
+
 	if Input.is_action_just_pressed("swim"):
+		swim_speed()
 		if !started:
 			start_swim()
-		swim_speed()
 
 	if position.y >= 500:
-		gravity_scale = 0
 		linear_velocity.y = 0
+		gravity_scale = 0
 		$AnimationPlayer.stop()
-
+		
 func start_swim():
 	started = true
-	gravity_scale = 5.0
+	gravity_scale = 5
 	$AnimationPlayer.play("swim")
 
 func swim_speed():
